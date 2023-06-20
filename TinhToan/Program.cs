decfbcca7634520d40001fa58a90b67bd974bb1f
hoang vu <vumonglep@gmail.com>
2023-06-20 16:27:37 +0700

    tinh toan

---
+++ TinhToan/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+//nhaapj 2 so a b tinh tong
+Console.WriteLine("mời nhập số a");
+var a = Int32.Parse(Console.ReadLine());
+Console.WriteLine("mời nhập số b");
+var b = Int32.Parse(Console.ReadLine());
+
+var tong = a + b;
+
+Console.WriteLine(tong);
+Console.WriteLine("Hello, World!");
